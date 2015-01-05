4c269bc488fd0cf0a85f4f755e35c4e455dff232
Eirik Arcanz Hanssen <eirikrahanssen@gmail.com>
2015-01-05 15:51:44 +0100

    fix

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake ()
+	{
+		players = FindObjectsOfType<Player>().ToList();
+	}
+
+	private List<Player> players;
+	private List<KeyCode> leftCodes;
+	private List<KeyCode> rightCodes; 
+
+	public List<KeyCode> LeftKeyCodes
+	{
+		get
+		{
+			if(leftCodes==null)
+				leftCodes = players.Select(player => player.leftKeyCode).ToList();
+			return leftCodes;
+		}
+	}
+
+	public List<KeyCode> RightKeyCodes
+	{
+		get { return rightCodes ?? (rightCodes = players.Select(player => player.rightKeyCode).ToList()); }
+	} 
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Scripts/Respawn.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+	public bool RespawnNow;
+	public GameObject RespawnableObject;
+	public float InvulnerableTime = 2;
+	public float BlinkFrequency = 0.2f;
+
+	private BoxCollider _objectCollider;
+	private bool _timeToRespawn;
+	private float _timerSeconds;
+
+	private float _blinkStartFrequency;
+	private bool _renederEnabled;
+
+	void Start()
+	{
+		_blinkStartFrequency = BlinkFrequency;
+		_timeToRespawn = false;
+		_timerSeconds = 0;
+		_renederEnabled = true;
+	}
+
+	void Update()
+	{
+		if (RespawnNow && !_timeToRespawn)
+		{
+			_objectCollider = RespawnableObject.GetComponent<BoxCollider>();
+			_objectCollider.enabled = false;
+			_timeToRespawn = true;
+		}
+
+		if (_timeToRespawn && _timerSeconds < InvulnerableTime)
+		{
+			_timerSeconds += Time.deltaTime;
+
+			if (_timerSeconds > _blinkStartFrequency)
+			{
+				_blinkStartFrequency += BlinkFrequency;
+				_renederEnabled = !_renederEnabled;
+				RespawnableObject.renderer.enabled = _renederEnabled;
+			}
+		}
+		else if (_timeToRespawn && _timerSeconds > InvulnerableTime)
+		{
+			Start();
+			_objectCollider.enabled = true;
+			RespawnableObject.renderer.enabled = true;
+			RespawnNow = false;
+		}
+	}
+}
