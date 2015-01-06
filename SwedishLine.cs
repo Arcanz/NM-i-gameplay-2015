903b23a7a07b88db5bfd72518630eff9ebc41dba
redomex <r3d0m3x@hotmail.com>
2015-01-06 16:51:24 +0100

    more swedish line

---
+++ SwedishLine.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwedishLine : MonoBehaviour {
+
+
+	void OnTriggerEnter(Collider playerCollider)
+	{
+		Application.LoadLevel(0);
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
