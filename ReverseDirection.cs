ea95595945378bc480879494c1501686d927da3d
redomex <r3d0m3x@hotmail.com>
2015-01-06 15:16:24 +0100

    added animations

--- ReverseDirection.cs
+++ ReverseDirection.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class ReverseDirection : MonoBehaviour {
 
+	bool havePunched = false;
+	float timer;
 	void OnTriggerEnter(Collider playerCollider)
 	{
+		animation.Play("Punch");
 		playerCollider.GetComponent<Player>().SetTempReverseDurection(2f);
-		Despawn();
+		havePunched = true;
 	}
 	void Despawn()
 	{
 		Destroy(gameObject);
+	}
+	void Update()
+	{
+		if(havePunched)
+			timer += Time.deltaTime;
+		if(timer > 1)
+			Despawn();
+		
 	}
 }
--- Root.cs
+++ Root.cs
@@ -2,14 +2,28 @@
 using System.Collections;
 
 public class Root : MonoBehaviour {
-
+	
+	bool notOpened = false;
+	float timer;
 	void OnTriggerEnter(Collider playerCollider)
 	{
-		playerCollider.GetComponent<Player>().SetRoot(2f);
-		Despawn();
+		
+		playerCollider.GetComponent<Player>().SetRoot(1.5f);
+		notOpened = true;
 	}
 	void Despawn()
 	{
 		Destroy(gameObject);
+	}
+	void Update()
+	{
+		if(notOpened)
+			timer += Time.deltaTime;
+		if(timer > 1.5f)
+			animation.Play("OpenRoot");
+			
+		if(timer > 2f)
+			Despawn();
+		
 	}
 }
