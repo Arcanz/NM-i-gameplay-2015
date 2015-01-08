c40b8104cc5cc876d8d1e4c266f13d9021878568
Jory <me@jory.org>
2015-01-08 19:00:41 +0100

    Added bear trap sounds

--- Assets/Scripts/Powerups/Root.cs
+++ Assets/Scripts/Powerups/Root.cs
@@ -7,10 +7,11 @@ public class Root : IPickupable
 	float timer;
 	void OnTriggerEnter(Collider playerCollider)
 	{
-		AudioManager.PlaySound("Beartrap", gameObject);
+		AudioManager.PlaySound("FX/Powerups/Bear-Trap", gameObject);
 		animation.Play("LockRoot");
 		playerCollider.GetComponent<Player>().SetRoot(1.5f);
 		notOpened = true;
+		AudioManager.PlaySound("FX/Powerups/Bear-Trap_reset", gameObject);
 	}
 
 	void Update()
